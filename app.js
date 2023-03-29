4bc7574e30f0921aae026c5cf66ec40ef313e5d1
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-03-29 17:43:09 -0400

    added search form

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.set('view engine', 'handlebars');
 app.set('views', './views');
 // Routes
 
-app.get('/', function (req, res) {
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    res.render('hello-gif', { gifUrl })
-});
+app.get('/', (req, res) => {
+    console.log(req.query) // => "{ term: hey" }[/bold]
+    res.render('home')
+  })
 
 app.get('/greetings/:name', (req, res) => {
     const name = req.params.name;
