7e2f9e15259d84b9b982f3ee1470f93a984299d6
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-03-29 16:58:52 -0400

    first route built

---
+++ app.js
@@ -0,0 +1,20 @@
+// Require Libraries
+const express = require('express');
+
+// App Setup
+const app = express();
+
+// Middleware
+
+// Routes
+app.get('/:username', (req, res) => {
+    // Here you would look up the user from the database
+    // Then render the template to display the users's info
+    res.send('Hello Squirrel');
+  })
+
+// Start Server
+
+app.listen(3000, () => {
+  console.log('Gif Search listening on port localhost:3000!');
+});
