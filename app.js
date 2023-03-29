cf17c9ef76b776dd02281d89256e474e918549bc
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-03-29 17:30:12 -0400

    adding middleware

--- app.js
+++ app.js
@@ -5,16 +5,31 @@ const express = require('express');
 const app = express();
 
 // Middleware
-
+const handlebars =  require('express-handlebars');
+const hbs = handlebars.create({
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        foo() { return 'FOO!'; },
+        bar() { return 'BAR!'; }
+    }
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 // Routes
-app.get('/:username', (req, res) => {
-    // Here you would look up the user from the database
-    // Then render the template to display the users's info
-    res.send('Hello Squirrel');
-  })
 
+app.get('/', function (req, res) {
+    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    res.render('hello-gif', { gifUrl })
+});
+
+app.get('/greetings/:name', (req, res) => {
+    const name = req.params.name;
+    res.render('greetings', { name });
+  })
+ 
 // Start Server
 
-app.listen(3000, () => {
-  console.log('Gif Search listening on port localhost:3000!');
+app.listen(3003, () => {
+  console.log('Gif Search listening on port localhost:3003!');
 });
